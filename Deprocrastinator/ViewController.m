24b131fd30b7ee8e0f30a1e924750e0b04425e85
Ceasar Barbosa <CeazB@Ceasars-MacBook-Pro.local>
2016-01-18 14:31:47 -0600

    Added outlets and IBAction

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *editButton;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property NSMutableArray *listArray;
 
 @end
 
@@ -16,12 +21,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.listArray = [NSMutableArray arrayWithObjects:
+                      @"Wash Car",
+                      @"Drink Beer",
+                      @"Dring More Beer",
+                      nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.listArray.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *) indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellID"];
+    cell.textLabel.text = [self.listArray objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+
+
+- (IBAction)onEditTapped:(UIBarButtonItem *)sender {
+}
+
+
+- (IBAction)onAddTapped:(UIButton *)sender {
 }
 
 @end
